a3dc4c17b8ab12ca9a40e95c6268dedea0742641
pintsizedsalt <samlisingisaac28@gmail.com>
2024-10-28 00:53:43 +0800

    yawqna

--- resources/views/edit-note.blade.php
+++ resources/views/edit-note.blade.php
@@ -41,7 +41,7 @@
 
             <h1>Edit Note</h1>
             <div class="back-home">
-            <a href="{{ route('showAll') }}" class="home-button"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M760-200v-160q0-50-35-85t-85-35H273l144 144-57 56-240-240 240-240 57 56-144 144h367q83 0 141.5 58.5T840-360v160h-80Z"/></svg>
+            <a href="{{ route('showNote', ['id' => $note->id]) }}" class="home-button"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M760-200v-160q0-50-35-85t-85-35H273l144 144-57 56-240-240 240-240 57 56-144 144h367q83 0 141.5 58.5T840-360v160h-80Z"/></svg>
             </a></div>
         </header>
         
